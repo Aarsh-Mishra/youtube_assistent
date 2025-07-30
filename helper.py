68440ab2abf62192f5844d61d0fbd3a99b68b643
Aarsh <aarshm21@gmail.com>
2025-07-30 22:26:14 +0530

    Format the code and import some more modules

--- helper.py
+++ helper.py
@@ -1,19 +1,21 @@
+import os
+from dotenv import load_dotenv
+
 from langchain.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain import PromptTemplate
-from langchain.chains import LLMChain
 from langchain.vectorstores import FAISS
-from langchain.embeddings import GoogleGenerativeAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
-from dotenv import load_dotenv
-import os
-import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-
+# Load environment variables from .env file
 load_dotenv()
 
+# Initialize the Google AI embedding model
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Initialize the Gemini language model
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest", temperature=0.7)
 
-
-model = genai.GenerativeModel('gemini-1.5-flash-latest')
